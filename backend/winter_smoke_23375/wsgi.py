b5d8b60df0bdffc4b9f6427ad321fe5871994c47
Crowdbotics <team@crowdbotics.com>
2020-12-14 22:05:28 +0000

    Initial commit.

---
+++ backend/winter_smoke_23375/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for winter_smoke_23375 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'winter_smoke_23375.settings')
+
+application = get_wsgi_application()
