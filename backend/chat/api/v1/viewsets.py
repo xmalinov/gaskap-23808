2d5d061cb44f90e1e04727a86afe7752f08f4c83
Brian Moranga <brian.moranga@infocap.co.ke>
2021-01-15 11:24:46 +0300

    Adds create chat

--- backend/chat/api/v1/viewsets.py
+++ backend/chat/api/v1/viewsets.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+from rest_framework import viewsets, status
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -8,16 +10,24 @@ from chat.models import Chat
 from chat.utils import get_user_contact
 
 
-class ChatViewSet(viewsets.ModelViewSet):
-    serializer_class = ChatSerializer
-    queryset = Chat.objects.all()
-    authentication_classes = (SessionAuthentication, TokenAuthentication)
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
+class ChatViewSet(viewsets.ViewSet):
+    def list(self, request):
         queryset = Chat.objects.all()
         username = self.request.query_params.get("username", None)
         if username is not None:
             contact = get_user_contact(username)
             queryset = contact.chats.all()
-        return queryset
+        serializer = ChatSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Chat.objects.all()
+        chat = get_object_or_404(queryset, pk=pk)
+        serializer = ChatSerializer(chat)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ChatSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
