b01a2375fcc85c9867196f961add8f12995e6505
Brian Moranga <brian.moranga@infocap.co.ke>
2021-01-19 10:46:42 +0300

    Updates routing import

--- backend/gaskap_23808/routing.py
+++ backend/gaskap_23808/routing.py
@@ -4,13 +4,13 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
 from chat.token_auth import TokenAuthMiddleware
-import chat.routing
+from chat import routing
 
 
 application = ProtocolTypeRouter(
     {
         "websocket": TokenAuthMiddleware(
-            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+            AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
         ),
     }
 )
