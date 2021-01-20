f671ccff598c53a2b1bc1897caa95ec27cf4a58b
Brian Moranga <brian.moranga@infocap.co.ke>
2021-01-20 20:10:54 +0300

    Adds redis host

--- backend/gaskap_23808/settings.py
+++ backend/gaskap_23808/settings.py
@@ -268,7 +268,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [(env.str("REDIS_URL"), 6379)],
         },
     },
 }
